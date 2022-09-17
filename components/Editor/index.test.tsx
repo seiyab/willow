04eaf2895001fa4a0ca870d9b12dbaf2d4834b0a
Seiya <20365512+seiyab@users.noreply.github.com>
2022-09-17 13:10:00 +0900

    setup test for ui (#8)

---
+++ components/Editor/index.test.tsx
@@ -0,0 +1,20 @@
+import assert from "assert";
+import { fireEvent, render, screen, within } from "@testing-library/react";
+import Editor from ".";
+import { drag } from "lib/testing";
+
+describe("<Editor />", () => {
+  it("draw a rect", async () => {
+    render(<Editor />);
+
+    const canvas = screen.getByTestId("canvas");
+
+    fireEvent.click(screen.getByRole("button", { name: "rect" }));
+
+    assert.equal(canvas.childElementCount, 0);
+
+    await drag(canvas).start({ x: 20, y: 40 }).end({ x: 100, y: 150 });
+
+    assert.equal(canvas.childElementCount, 1);
+  });
+});
